f552e04a157e0e61ba29306a21a96c73373b2c3c
mohammad bagherani <mohammad.bagherani@gmail.com>
2023-07-05 16:53:24 +0330

    tests improved

--- server/ai/index.test.ts
+++ server/ai/index.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "bun:test";
 import { extractFlightQuery } from "./index";
+import { FlightInfo } from "../models";
 
-test("chat gpt command test", async () => {
-  const resp = await extractFlightQuery(
+test("should give a valid json for flights", async () => {
+  const result = await extractFlightQuery(
     "flight from Tehran to Paris for September 8th for 1 adult and one way"
   );
-  console.log(resp);
 
-  expect(1).toBe(1);
+  const flightInfo: FlightInfo = JSON.parse(result.data.object);
+  expect(flightInfo.from).toBe("Tehran");
+  expect(flightInfo.to).toBe("Paris");
+  expect(flightInfo.date).toBe("2023-09-08");
+  expect(flightInfo.adults).toBe(1);
+  expect(flightInfo.twoWay).toBe(false);
 });
