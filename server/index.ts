85fb6737607cf47f37a4e302d7f5e6b9bcff9f32
mohammad bagherani <mohammad.bagherani@gmail.com>
2023-07-10 11:37:53 +0200

    default port updated

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { extractFlightQuery } from "./ai";
 import { has as hasEntry, get as getEntry, set as setEntry } from "./store";
 
 const server = Bun.serve({
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3010,
   async fetch(req) {
     const requestBody = await req.text();
 
