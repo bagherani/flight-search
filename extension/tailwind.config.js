a81b9722c876dd56ce8c60b2901d476c5dfb263a
mohammad bagherani <mohammad.bagherani@gmail.com>
2023-07-13 14:23:13 +0200

    chrome folder added to tailwind config

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./angular/src/**/*.{html,js,ts}"],
+  content: ["./angular/src/**/*.{html,js,ts}", "./chrome/src/**/*.{ts}"],
   theme: {
     extend: {},
   },
