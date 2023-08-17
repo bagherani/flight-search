648598d6398f2fb3c3738b3c77cfe6d8e687c770
bagherani <mohammad.bagherani@gmail.com>
2023-08-17 16:39:46 +0200

    [wip] audio capture browser api added

--- extension/angular/src/app/modules/popup/pages/popup/popup.component.ts
+++ extension/angular/src/app/modules/popup/pages/popup/popup.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject } from '@angular/core';
 
 import { TAB_ID } from '../../../../providers/tab-id.provider';
 
+const SpeechRecognition =
+  (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+
 @Component({
   selector: 'app-popup',
   templateUrl: 'popup.component.html',
@@ -17,4 +20,38 @@ export class PopupComponent {
 
     chrome.tabs.sendMessage(this.tabId, this.userCommand);
   }
+
+  handleVoiceCommand() {
+    chrome.permissions.request(
+      {
+        permissions: ['audioCapture']
+      },
+      (granted: boolean) => {
+        if (granted) {
+          recognition.start(); // Start speech recognition
+          console.log('Microphone permission granted');
+          // Call the code that starts using the microphone here
+        } else {
+          console.log('Microphone permission denied');
+        }
+      }
+    );
+  }
 }
+
+const recognition = new SpeechRecognition();
+recognition.continuous = true; // Set to true for continuous speech recognition
+recognition.lang = 'en-US'; // Set the language for speech recognition
+
+recognition.onresult = (event: any) => {
+  const transcript = event.results[event.results.length - 1][0].transcript;
+  console.log(`You said: ${transcript}`);
+};
+
+recognition.onerror = (event: any) => {
+  console.error(`Speech recognition error: ${event.error}`);
+};
+
+recognition.onend = () => {
+  console.log('Speech recognition ended');
+};
